e29e8f5fd4b17b16d5aa23bcbe850b2b27186014
Quân Đinh <17819030+urjhams@users.noreply.github.com>
2023-11-19 00:46:43 +0100

    WIP - moving pupil diamter part into new file

---
+++ BreathActivity/Essential/TobiiTracker.swift
@@ -0,0 +1,87 @@
+import Combine
+
+public class TobiiTracker: NSObject, ObservableObject {
+  /// Store the average value of Pupil diameters (left and right eye)
+  public var avgPupilDiameter = PassthroughSubject<Float, Never>()
+  
+  let process = Process()
+  
+  init() {
+    setupProcess()
+  }
+}
+
+extension TobiiTracker {
+  private func setupProcess() {
+    /* Bash command:
+     arch -x86_64 /usr/local/homebrew/bin/python3.10 [script.py in the bundle]
+     */
+    
+    guard
+      let scriptPath = Bundle.main.path(forResource: "Python/script", ofType: "py")
+    else {
+      return
+    }
+    // because the python script will need to run as x86_64 architechture
+    // we need a bit config for the command
+    
+    // mac M1 default python (installed by homebrew): "/usr/local/bin/python3"
+    // this is the python we installed via x86_64 context
+    let python = "/usr/local/homebrew/bin/python3.10"
+    
+    let command = "arch -x86_64 \(python) \(scriptPath)"
+    
+    // progress configuration
+    process.arguments = ["-c", command]
+    process.executableURL = URL(fileURLWithPath: "/bin/zsh")
+  }
+  
+  public func startReadPupilDiameter() {
+    prepareToReadPupilDiameter()
+    
+    do {
+      try process.run()
+    } catch {
+      print(error.localizedDescription)
+      return
+    }
+  }
+  
+  private func prepareToReadPupilDiameter() {
+    // install pipline for data output comunication
+    let pipe = Pipe()
+    process.standardOutput = pipe
+    
+    let fileHandle = pipe.fileHandleForReading
+    fileHandle.waitForDataInBackgroundAndNotify()
+    
+    // Notificaiton center observer for the data output
+    NotificationCenter.default.addObserver(
+      forName: .NSFileHandleDataAvailable,
+      object: fileHandle,
+      queue: nil
+    ) { _ in
+      let data = fileHandle.availableData
+      if data.count > 0 {
+        // we expect the echo command will show something like '{value}\n'
+        // so we need to remove the newLine by dropLast
+        if let output = String(data: data, encoding: .utf8)?.dropLast() {
+          if let float = Float(String(output)) {
+            avgPupilDiameter.send(float)
+          } else {
+            print(output)
+          }
+        }
+        fileHandle.waitForDataInBackgroundAndNotify()
+      } else {
+        // terminated state or fail state
+        print("terminated state")
+      }
+    }
+
+  }
+  
+  public func stopReadPupilDiameter() {
+    process.terminate()
+  }
+}
