7bd693948eb6a9e84246207da8fbfceef3ea8732
Quân Đinh <17819030+urjhams@users.noreply.github.com>
2024-01-23 23:20:22 +0100

    Update GraphView.swift

--- BreathActivity/Views/GraphView.swift
+++ BreathActivity/Views/GraphView.swift
@@ -1,18 +1,36 @@
-//
-//  GraphView.swift
-//  BreathActivity
-//
-//  Created by Quân Đinh on 23.01.24.
-//
-
 import SwiftUI
+import Charts
 
 struct GraphView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  
+  @State var amplitudeData = [Float]()
+  @State var pupilData = [Float]()
+  
+  var body: some View {
+    VStack {
+      Chart(Array(amplitudeData.enumerated()), id: \.0) { index, magnitude in
+        LineMark(x: .value("id", index), y: .value("amplitude", magnitude))
+          .foregroundStyle(.red)
+      }
+      .chartXAxis(.hidden)
+      .padding(.all)
+      
+      Chart(Array(pupilData.enumerated()), id: \.0) { index, magnitude in
+        LineMark(x: .value("id", index), y: .value("pupil", magnitude))
+          .foregroundStyle(.green)
+      }
+      .chartXAxis(.hidden)
+      .padding(.all)
     }
+    .onAppear {
+      if let storage = IOManager.tryToRead(from: "Ngoc Anh - easy") {
+        amplitudeData = storage.collectedData.map(\.amplitude)
+        pupilData = storage.collectedData.map(\.pupilSize)
+      }
+    }
+  }
 }
 
 #Preview {
-    GraphView()
+  GraphView()
 }
