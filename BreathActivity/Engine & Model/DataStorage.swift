69d3ce4cb9e2abba29a6dd16713b59fefba205ce
Quân Đinh <17819030+urjhams@users.noreply.github.com>
2024-03-04 14:50:36 +0100

    workaround to silent warning because of @Observable with Codable

--- BreathActivity/Engine & Model/DataStorage.swift
+++ BreathActivity/Engine & Model/DataStorage.swift
@@ -20,11 +20,19 @@ public struct Response: Codable {
   var reaction: ReactionType
 }
 
-@Observable internal class DataStorage: Codable {
+@Observable
+internal class DataStorage: Codable {
   var candidateName: String = ""
   var level: String = ""
   var collectedData: [CollectedData] = []
   var responses: [Response] = []
+  
+  enum CodingKeys: String, CodingKey {
+    case _candidateName = "candidateName"
+    case _level = "level"
+    case _collectedData = "collectedData"
+    case _responses = "responses"
+  }
   
   public func reset() {
     level = ""
